6b074aa62b0b27df031d775e017472a66825a3be
Edineia de Almeida <edineia.almeida@docente.fieb.edu.br>
2026-06-17 08:52:42 -0300

    first commit

---
+++ src/main/java/br/itb/projeto/pizzaria/controller/MessageController.java
@@ -0,0 +1,41 @@
+package br.itb.projeto.pizzaria.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+ 
+@Controller
+public class MessageController {
+ 
+    @GetMapping("/home")
+    public String paginaInicial(Model model) {
+ 
+        model.addAttribute("titulo", "Início");
+        model.addAttribute("mensagem", "Bem-vindo a Pizzaria ITB!");
+        model.addAttribute("paginaAtual", "home");
+        return "layout";
+    }
+ 
+     @GetMapping("/cardapio")
+    public String cardapio(Model model) {
+ 
+        model.addAttribute("titulo", "Cardápio");
+        model.addAttribute("paginaAtual", "cardapio");
+        return "cardapio";
+    }
+      @GetMapping("/sobre")
+    public String sobre(Model model) {
+ 
+        model.addAttribute("titulo", "Sobre nós");
+        model.addAttribute("paginaAtual", "sobre");
+        return "sobre";
+    }
+ 
+        @GetMapping("/contato")
+    public String contato(Model model) {
+ 
+        model.addAttribute("titulo", "Fale Conosco");
+        model.addAttribute("paginaAtual", "contato");
+        return "contato";
+    }
+}
